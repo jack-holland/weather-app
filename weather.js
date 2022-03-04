aa60e944193b135b37b8cb21a6e92e262cca2449
Jack <jackholl@me.com>
2022-03-04 22:42:34 +0000

    Created function to get Weather data from location ID

--- weather.js
+++ weather.js
@@ -2,23 +2,42 @@ $("form").submit(function(e) {
     e.preventDefault();
 
     if($("#location").val().length === 0) {
-        $("small").fadeIn().removeClass("d-none");
+        // Error message for empty input
+        $("small").fadeIn().removeClass("d-none").append("You must ender a location.");
+    } else {
+        $location = $("#location").val();
+        getWeatherId($location);
     }
 
-    $location = $("#location").val();
-    getWeatherId($location);
-
-    function getWeatherId($location) {
+    function getWeatherId(location) {
         // AccuWeather API Key
         $API_KEY = "w1cLV9dDNDnv5L9SKOW1YsP8a8t0rt8m";
 
-        $.getJSON( "https://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + $API_KEY +"&q="+ $location +"", function(data) {
+        $.getJSON("https://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + $API_KEY +"&q="+ $location +"", function(data) {
          }).done(function(data) {
-             return data[0].Key; // Get the first location ID 
+            if(data.length > 1)
+            {  
+                getWeather(data[0].Key); // Get the first location  
+            } else {
+                // Unable to get location
+                $("small").fadeIn().removeClass("d-none").append("No location found.");
+            }
          })
          .fail(function() {
-           console.log( "Error with fetching location data" );
+             // Error grabbing JSON
+             $("small").fadeIn().removeClass("d-none").append("Unable to fetch weather, please try again later.");
          });
+    }
+
+    function getWeather(locationId) {
+        $.getJSON("https://dataservice.accuweather.com/forecasts/v1/hourly/1hour/"+ locationId +"?apikey="+ $API_KEY +"&metric=true", function(data) {
+        }).done(function(data) {
+            console.log(data);
+        })
+        .fail(function() {
+            // Error grabbing JSON
+            $("small").fadeIn().removeClass("d-none").append("Unable to fetch weather, please try again later.");
+        });
     }
 
 });
